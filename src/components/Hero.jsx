3498c07e8b923992bc42678602f5a7cd472308e0
Sanjana <sanjana.js02@gmail.com>
2024-03-06 14:20:01 +0100

    working

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,6 +15,7 @@ const Hero = () => {
         </div>
 
         <div>
+       
           <h1 className={`${styles.heroHeadText} text-white mb-4`}>
             Hello, I'm <span className="text-[#5d3da2]">Sanjana</span>
           </h1>
@@ -36,28 +37,28 @@ const Hero = () => {
             </a>
             , validating my expertise in software testing. Further enhancing my
             capabilities, I successfully completed a rigorous bootcamp at
-            Ironhack, specializing in Full Stack Web development.{" "}
-            <br className="sm:block hidden" /> Through this program, I gained
+            Ironhack, specializing in Full Stack Web development.Through this program, I gained
             proficiency in a wide array of technologies including HTML, CSS,
             JavaScript, Node.js, Express.js, React.js, and MongoDB.
             <br className="sm:block hidden" />{" "}
             <br className="sm:block hidden" />
           </p>
-        </div>
-        <div className="flex justify-end mt-2">
+          <div className="flex justify-center mt-2">
           <img
             src={photo}
             alt="Sanjana image"
             style={{
               borderRadius: "20%", // Make the image circular
-              marginTop: "50px", // Add margin from the top
-              width: "100%", // Default width for larger screens
-              maxWidth: "600px", // Set maximum width for larger screens
+              marginTop: "5px", // Add margin from the top
+             // width: "150%", // Default width for larger screens
+              maxWidth: "150px", // Set maximum width for larger screens
               height: "auto", // Maintain aspect ratio
             }}
             className="mobile-image" // Add a class for targeting in CSS
           />
         </div>
+        </div>
+      
       </div>
     </section>
   );
